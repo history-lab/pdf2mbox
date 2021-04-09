12dd7f10907ed2476b3b8c0c564e26c37c1b2ac7
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2021-04-09 09:32:54 -0400

    Major refactoring of classes in pdfemail.py.

--- pdfemail.py
+++ pdfemail.py
@@ -1,20 +1,6 @@
 from dataclasses import dataclass, field
 from typing import ClassVar
-
-
-@dataclass
-class HeaderParser:
-    field_tokens:   ClassVar[list[str]] = ['from', 'to', 'cc', 'bcc',
-                                           'subject', 'date', 'attachments']
-    max_start:      ClassVar[int] = 12
-    header:         {}
-
-    def tokenizer(self, line):
-        """Tokenizes a string if it represents an element of an email header"""
-        line = line.strip()
-        loc = line.find(':')
-        if loc != -1:           # found add to header dictionary
-            self.header[line[:loc].lower()] = line[loc+1:].strip()
+from collections import defaultdict
 
 
 @dataclass
@@ -26,15 +12,146 @@ class Header:
     cc:             list[str] = field(default_factory=list)
     bcc:            list[str] = field(default_factory=list)
     attachments:    list[str] = field(default_factory=list)
+    begin_ln:       int = field(default_factory=int)   # start line number
+    end_ln:         int = field(default_factory=int)   # finish line number
 
 
 @dataclass
-class Email:
-    header:         Header
+class Page:
     body:           str
 
 
+@dataclass
+class Email(Page):
+    header:         Header
+    pdf_filename:   str = field(default_factory=str)
+    page_number:    int = field(default_factory=int)
+    page_count:     int = field(default_factory=int)
+
+
+@dataclass
+class HeaderParser:
+    pgarr:          list[str]
+    _FIELD_TOKENS:  ClassVar[list[str]] = ['from', 'to', 'cc', 'bcc',
+                                           'subject', 'date', 'attachments']
+    _MAX_START:     ClassVar[int] = 12
+    _header:        defaultdict(str) = field(
+        default_factory=lambda: defaultdict(str))
+    _ln:            int = 0                          # line position in page
+    _token:         str = field(default_factory=str)  # last field token
+    _lncnt:         int = 0                           # len(pgarr)
+
+    def _find_start(self):
+        self._ln = 0
+        while (self._ln < self._MAX_START and self._ln < self._lncnt and
+               self.pgarr[self._ln].find(':') == -1):
+            self._ln += 1
+            # print(f'in find start loop - ln: {self._ln}')
+        if self._ln == self._MAX_START or self._ln == self._lncnt:
+            return False    # Reached max header start position or page end
+        else:
+            self._header['begin_ln'] = self._ln
+            return True     # Found the start of header
+
+    def _tokenize(self):
+        """Tokenizes a string if it represents an element of an email header"""
+        line = self.pgarr[self._ln].strip()
+        print('in tokenize: {line}')
+        loc = line.find(':')
+        if loc != -1:           # found add to header dictionary
+            self._token = line[:loc].lower()
+            if self._token in self._FIELD_TOKENS:
+                self._header[self._token] = line[loc+1:].strip()
+            else:
+                print(f'Warning - unprocessed header element: {self._token}')
+        elif self._token in self.field_tokens:
+            # existing token value carried onto next line
+            self._header[self._token] += line
+
+    def _next_line(self):
+        """Returns True if there is another line in the header.
+           False otherwise. Side effect: always increments ln."""
+        self._ln += 1
+        if self._ln >= self._lncnt:                # Reached end of page
+            return False
+        elif self.pgarr[self._ln].strip() == '':   # Blank line indicates EOH
+            return False
+        else:
+            return True
+
+    def _convert_obj(self):
+        """Create a PDFEmailHeader object based on the contents of the
+        self.header dictionary. If required fields are missing, it raises
+        warnings and returns None."""
+        return Header(from_email=self._header['from'],
+                      to=self._header['to'],
+                      cc=self._header['cc'],
+                      bcc=self._header['bcc'],
+                      subject=self._header['subject'],
+                      date=self._header['date'],
+                      attachments=self._header['attachments'],
+                      begin_ln=self._header['begin_ln'],
+                      end_ln=self._header['end_ln'])
+
+    def parse(self):
+        self._lncnt = len(self.pgarr)
+        # find the start of the header
+        if self._find_start():
+            print('find start returned true')
+            while True:
+                self._tokenize()
+                if not self._next_line():
+                    break
+            self._header['end_ln'] = self._ln
+            header_obj = self._convert_obj()
+            print(f'header_obj: {type(header_obj)}')
+            return header_obj
+        else:          # No header
+            return None
+
+
+def parse(page):
+    """Parses a string representing a PDF page. If it detects an email"""
+    pgarr = page.splitlines()
+    hp = HeaderParser(pgarr)
+    header = hp.parse()
+    if header:
+        body = '\n'.join(pgarr[header.end_ln::])
+        return Email(header=header, body=body)
+    else:
+        body = '\n'.join(pgarr)
+        return Page(body=body)
+
+
 def main():
+    example_page = """
+    From:       yogi.bear@cartoon.com
+    To:         booboo.bear@cartoon.com
+    Subject:    this afternoon
+    Date:       Thursday, March 25, 2021 06:16:10 AM
+    Status:     Urgent
+
+    Hi Booboo:
+
+    Let's go see the ranger this afternoon at 2 o'clock, ok?
+
+    Yogi
+    """
+    pg = parse(example_page)
+    print(type(pg))
+    print(pg)
+    example_page = """
+    This is an example of a continuation page, which occurs when an email
+    extends beyond a page.
+
+    Thanks,
+    Yogi
+    """
+    pg = parse(example_page)
+    print(type(pg))
+    print(pg)
+
+    exit()
     e1 = Email(header=Header(from_email='yogi.bear@cartoon.com',
                              to=['booboo.bear@cartoon.com'],
                              subject='this afternoon',
