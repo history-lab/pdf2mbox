ca760be4e6dab9917193da694374a12481c0b3e4
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2021-08-06 11:31:22 -0400

    Added PDF file level exception handling

--- pdfparse.py
+++ pdfparse.py
@@ -1,20 +1,23 @@
 import pgparse
 import pdftotext
 import magic
-from os.path import basename
 
 
 class PDF:
     def __init__(self, file_id, pdf_filename):
         self.file_id = file_id
         self.filename = pdf_filename
-        self.filetype = magic.from_file(self.filename)
+        self.filetype = ''
+        self.pgcnt = 0
         self.emails = []
+        self.error = ''
         # convert to text
-        with open(self.filename, 'rb') as f:
-            self.pdf = pdftotext.PDF(f)
-            self.pgcnt = len(self.pdf)
-        self.__parse()
+        try:
+            self.filetype = magic.from_file(self.filename)
+            self.__pdf2txt()
+            self.__parse()
+        except Exception as e:
+            self.error = str(e)
 
     def __parse(self):
         i = 0
@@ -35,7 +38,16 @@ class PDF:
             if current_email:   # write last email
                 self.emails.append(current_email)
 
+    def __pdf2txt(self):
+        with open(self.filename, 'rb') as f:
+            self.pdf = pdftotext.PDF(f)
+            self.pgcnt = len(self.pdf)
+
     def get_summary(self):
-        return f'{self.file_id}) {basename(self.filename)}: ' \
+        if self.error:
+            error_str = ', ' + self.error
+        else:
+            error_str = self.error
+        return f'{self.file_id}) {self.filename}: ' \
                f'{self.filetype}; ' \
-               f'{self.pgcnt} pages, {len(self.emails)} emails'
+               f'{self.pgcnt} pages, {len(self.emails)} emails{error_str}'
