a3db2db6e59dcaff8b502a3204c9237b5b5aa756
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2021-08-16 16:50:49 -0400

    Added call to upsert_file_stats.

--- pdf2db.py
+++ pdf2db.py
@@ -1,23 +1,31 @@
 import aiosql
 import psycopg2
 import os
-# import pdfemail         # right now it's a symbolic link to pdf2mbox
+import pdfparse
 
 PDFDIR = os.getenv('PDFDIR')
 conn = psycopg2.connect("")
 conn.autocommit = True
-stmts = aiosql.from_path("pdf2db.sql", "psycopg2")
+db = aiosql.from_path("pdf2db.sql", "psycopg2")
 
-# pdfs = sql.get_dc19pdf_list(conn)
-# for p in pdfs:
-#    print(p[1])
 
-#
-# pdf_list = os.listdir(PDFDIR)
-# print(pdf_list)
-#
-stmts.insert_email(conn, file_id=32, file_pg_start=1, pg_cnt=4,
-                   header_begin_ln=2, header_end_ln=8, from_email='xx@abc.com',
-                   to_emails='yyy@def.com', cc_emails='zzz@def.com',
-                   attachments='a.pdf', importance='', subject='test email',
-                   body='line1\nline2\nline3')
+def add_emails(file_id, email):
+    pass
+
+
+pdfs = db.get_dc19pdf_list(conn)
+for p in pdfs:
+    f = pdfparse.PDF(PDFDIR + '/' + p[1], p[0])
+    print(f.get_summary())
+    print(type(f.file_id), type(f.pgcnt), type(len(f.emails)),
+          type(f.filetype), type(f.error))
+    db.upsert_file_stats(conn, file_id=f.file_id, pg_cnt=f.pgcnt,
+                         email_cnt=len(f.emails), type_desc=f.filetype,
+                         error_msg=f.error)
+
+#  stmts.insert_email(conn, file_id=32, file_pg_start=1, pg_cnt=4,
+#                     header_begin_ln=2, header_end_ln=8,
+#                     from_email='xx@abc.com',
+#                     to_emails='yyy@def.com', cc_emails='zzz@def.com',
+#                     attachments='a.pdf', importance='', subject='test email',
+#                     body='line1\nline2\nline3')
