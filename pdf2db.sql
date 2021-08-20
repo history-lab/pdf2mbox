237e6df01545d09b5293615005f7db643d78a67b
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2021-08-20 15:59:11 -0400

    Added get-emails-in-file query

--- pdf2db.sql
+++ pdf2db.sql
@@ -13,6 +13,14 @@ values (:file_id, :file_pg_start, :pg_cnt, :header_begin_ln,
     :header_end_ln, :from_email, :to_emails, :cc_emails, :bcc_emails,
     :attachments, :importance, :subject, :sent, :body, :header_unprocessed);
 
+-- name: get-emails-in-file
+-- Select all emails from a specific file, identified by id.
+select email_id, file_pg_start, pg_cnt, header_begin_ln, header_end_ln,
+    from_email, to_emails, cc_emails, bcc_emails, attachments, importance,
+    subject, sent, body, header_unprocessed
+from covid19.emails
+where file_id = :file_id;
+
 -- name: upsert-file-stats!
 -- Upsert file_stats
 insert into covid19.file_stats (file_id, pg_cnt, email_cnt, type_desc, error_msg)
