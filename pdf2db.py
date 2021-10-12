0602dcaf5b0ad70d33fe4cee8847cc72a64c18cc
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2021-10-12 10:45:22 -0400

    Inserts emails into db for all files returned by get_dc19pdf_list()

--- pdf2db.py
+++ pdf2db.py
@@ -8,21 +8,27 @@ conn = psycopg2.connect("")
 conn.autocommit = True
 db = aiosql.from_path("pdf2db.sql", "psycopg2")
 
-
-
-
 pdfs = db.get_dc19pdf_list(conn)
 for p in pdfs:
     f = pdfparse.PDF(PDFDIR + '/' + p[1], p[0])
     print(f.get_summary())
-
     db.upsert_file_stats(conn, file_id=f.file_id, pg_cnt=f.pgcnt,
                          email_cnt=len(f.emails), type_desc=f.filetype,
                          error_msg=f.error)
-
-#  stmts.insert_email(conn, file_id=32, file_pg_start=1, pg_cnt=4,
-#                     header_begin_ln=2, header_end_ln=8,
-#                     from_email='xx@abc.com',
-#                     to_emails='yyy@def.com', cc_emails='zzz@def.com',
-#                     attachments='a.pdf', importance='', subject='test email',
-#                     body='line1\nline2\nline3')
+    for e in f.emails:
+        # print(e.get_summary())  - TODO: make this run with verbose
+        try:
+            db.insert_email(conn, file_id=f.file_id,
+                            file_pg_start=e.page_number, pg_cnt=e.page_count,
+                            header_begin_ln=e.header.begin_ln,
+                            header_end_ln=e.header.end_ln,
+                            from_email=e.header.from_email,
+                            to_emails=e.header.to,
+                            cc_emails=e.header.cc, bcc_emails=e.header.bcc,
+                            attachments=e.header.attachments,
+                            importance=e.header.importance, sent=e.header.date,
+                            subject=e.header.subject, body=e.body,
+                            header_unprocessed=e.header.unprocessed)
+        except Exception as exc:
+            print(f'{e.page_number}: {exc}')
+            continue
