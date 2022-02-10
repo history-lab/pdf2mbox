6d7ed7fa3e6055c20222afc7b3ab690e9d0ed030
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2022-02-10 15:18:23 -0500

    Moved CLI code into cli() function and added __name == '__main__' conditional

--- pdf2mbox.py
+++ pdf2mbox.py
@@ -43,46 +43,51 @@ def pdf2mbox(pdf_filename, mbox_filename):
 
 
 # CLI
-parser = argparse.ArgumentParser(
-            description='Generates an mbox from a PDF containing emails')
-parser.add_argument('pdf_file', help='PDF file provided as input')
-parser.add_argument('mbox_file', nargs='?', default='out.mbox',
-                    help='Mbox file generated as output')
-parser.add_argument('--version', '-v', action='version',
-                    version=f"%(prog)s {importlib.metadata.version('pdf2mbox')}")
-parser.add_argument('--overwrite', '-o', action="store_true",
-                    help='overwrite MBOX file if it exists')
-parser.add_argument('--csv', nargs='?', const='out.csv',
-                    type=argparse.FileType('w', encoding='utf-8'),
-                    help='generate CSV file output')
-cl_args = parser.parse_args()
-pdf_filename = cl_args.pdf_file
-csv_filename = cl_args.csv
-mbox_filename = cl_args.mbox_file
-mbox_overwrite = cl_args.overwrite
-
-# File handling
-# PDF file
-if not os.path.exists(pdf_filename):
-    sys.exit(f'error: {pdf_filename} does not exist.')
-if not os.path.isfile(pdf_filename):
-    sys.exit(f'error: {pdf_filename} is not a file.')
-if magic.from_file(pdf_filename, mime=True) != 'application/pdf':
-    sys.exit(f'error: {pdf_filename} is not a PDF file.')
-# mbox
-if os.path.exists(mbox_filename):
-    if mbox_overwrite:
-        print(f'Overwriting {mbox_filename}')
-        os.remove(mbox_filename)
+def cli():
+    parser = argparse.ArgumentParser(description='Generates an mbox from a PDF \
+    containing emails')
+    parser.add_argument('pdf_file', help='PDF file provided as input')
+    parser.add_argument('mbox_file', nargs='?', default='out.mbox',
+                        help='Mbox file generated as output')
+    parser.add_argument('--version', '-v', action='version',
+                        version=f"%(prog)s \
+                        {importlib.metadata.version('pdf2mbox')}")
+    parser.add_argument('--overwrite', '-o', action="store_true",
+                        help='overwrite MBOX file if it exists')
+    parser.add_argument('--csv', nargs='?', const='out.csv',
+                        type=argparse.FileType('w', encoding='utf-8'),
+                        help='generate CSV file output')
+    cl_args = parser.parse_args()
+    pdf_filename = cl_args.pdf_file
+    csv_filename = cl_args.csv
+    mbox_filename = cl_args.mbox_file
+    mbox_overwrite = cl_args.overwrite
+    # File handling
+    # PDF file
+    if not os.path.exists(pdf_filename):
+        sys.exit(f'error: {pdf_filename} does not exist.')
+    if not os.path.isfile(pdf_filename):
+        sys.exit(f'error: {pdf_filename} is not a file.')
+    if magic.from_file(pdf_filename, mime=True) != 'application/pdf':
+        sys.exit(f'error: {pdf_filename} is not a PDF file.')
+    # MBOX
+    if os.path.exists(mbox_filename):
+        if mbox_overwrite:
+            print(f'Overwriting {mbox_filename}')
+            os.remove(mbox_filename)
+        else:
+            print(f'Appending email messages to {mbox_filename}')
     else:
-        print(f'Appending email messages to {mbox_filename}')
-else:
-    print(f'Writing email messages in MBOX format to {mbox_filename}')
-# csv
-if csv_filename:
-    print(f'Writing csv to {csv_filename.name}')
-#
-xms = pdf2mbox(pdf_filename, mbox_filename)
-print(xms.info())
-if csv_filename:
-    xms.to_csv(csv_filename)
+        print(f'Writing email messages in MBOX format to {mbox_filename}')
+    # csv
+    if csv_filename:
+        print(f'Writing csv to {csv_filename.name}')
+    return pdf_filename, mbox_filename, csv_filename
+
+
+if __name__ == '__main__':
+    pdf_filename, mbox_filename, csv_filename = cli()
+    xms = pdf2mbox(pdf_filename, mbox_filename)
+    print(xms.info())
+    if csv_filename:
+        xms.to_csv(csv_filename)
